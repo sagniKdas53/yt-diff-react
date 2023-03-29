5ddf3f81150b9a3cbeadd707a91f4262fc93b487
Sagnik Das <mailsagnikdas53@gmail.com>
2023-03-29 16:12:43 +0530

    the UI looks good

--- TODO.md
+++ TODO.md
@@ -1,7 +1,18 @@
 # TODO
 
-- [ ] Add a debounce to search [Tutorial](https://javascript.plainenglish.io/debounce-search-feature-in-react-46dfc31aede2#04a6)
-- [ ] Make the table headers sticky
-- [ ] Add input validation
+- [x] Add a debounce to search [Tutorial](https://javascript.plainenglish.io/debounce-search-feature-in-react-46dfc31aede2#04a6)
+- [x] Make the table headers sticky
+- [x] Add input validation
+- [ ] Add a dark theme
+  - [ ] See [this](https://m2.material.io/design/color/dark-theme.html)
+  - [ ] And use scss
 - [ ] Make the state of children change properly
+  - [ ] Not sure if I am doing it correctly
 - [ ] Make components handle dependencies elegantly
+  - [ ] Same not sure as of now
+- [ ] Make usre that the server is updating playlists properly now that there are so many ways to update them
+  - [ ] Find and fix why updating a playlist causes the old indecies to get jumbled up
+  - [ ] Check the Android Utopia playlist to see it in action
+- [ ] Fix the cron job to update as per the new requiremnts
+- [ ] Lastly figure out how to integrate the react app with the old server
+  - [ ] Maybe rewriting the server is the best way to integrate the react app.
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -12,44 +12,34 @@ export default function InputForm({ setParentUrl, setRespStart }) {
     const [start, setStart] = useState(0);
     const [stop, setStop] = useState(10);
     const [chunk, setChunk] = useState(10);
-    const [bulkListing, toggleBulk] = useState(false);
-    const [url, setUrl] = useState("");
+    //const [multiple, toggleMultiple] = useState(false);
+    //const [url, setUrl] = useState("");
     const [urlList, setUrlList] = useState("");
     const [watchMode, setWatch] = useState("1");
 
     const updateUrls = (event) => {
-        if (bulkListing) {
-            setUrlList(event.target.value);
-        } else {
-            setUrl(event.target.value);
-        }
+        //console.log(urlList.split("\n").length + 1);
+        setUrlList(event.target.value);
     };
+    const clearInput = () => {
+        setStart(0);
+        setStop(10);
+        setChunk(10);
+        setUrlList("");
+        setWatch("1");
+    }
     const listThis = async () => {
-        if (bulkListing) {
-            const valid = new Set(urlList.trim().split("\n").filter(validate));
-            try {
-                for (const element of valid) {
-                    const response = await postUrl(element).then((response) => response.text()).then((data) => JSON.parse(data));
-                    // console.log(response.rows[0]["reference"]);
-                    // since listing may take a while having this here as an intermediate state can't hurt too much.
-                    setParentUrl(response.rows[0]["reference"]);
-                    setRespStart(+response.start);
-                }
-            } catch (error) {
-                console.error(error);
+        const valid = new Set(urlList.trim().split("\n").filter(validate));
+        try {
+            for (const element of valid) {
+                const response = await postUrl(element).then((response) => response.text()).then((data) => JSON.parse(data));
+                // console.log(response.rows[0]["reference"]);
+                // since listing may take a while having this here as an intermediate state can't hurt too much.
+                setParentUrl(response.rows[0]["reference"]);
+                setRespStart(+response.start);
             }
-        } else {
-            try {
-                if (validate(url)) {
-                    const response = await postUrl(url).then((response) => response.text()).then((data) => JSON.parse(data));
-                    // I plan on getting the limits form the backend db thus being able to update the subList sate form here
-                    // fetching data as needed, this will also prevent the overhead of serealizing the resposne in intial listing.
-                    setParentUrl(response.rows[0]["reference"]);
-                    setRespStart(+response.start);
-                }
-            } catch (error) {
-                console.error(error);
-            }
+        } catch (error) {
+            console.error(error);
         }
     };
 
@@ -81,30 +71,24 @@ export default function InputForm({ setParentUrl, setRespStart }) {
                 stop: stop,
                 chunk: chunk,
                 watch: watchMode,
-                continuous: bulkListing
+                continuous: true
             })
         });
     }
-
-    /*useEffect(() => {
-        console.log(`MainList:\n\tbulk: ${bulkListing}\n\turl: "${url}"\n\turlList: "${urlList}"
-        start: ${start}\n\tstop: ${stop}\n\tchunk: ${chunk}\n\twatch: ${watchMode}`);
-        //\n\titems: ${JSON.stringify(items)}
-    }, [bulkListing, url, urlList, watchMode, chunk, start, stop])*/
 
     return (
         <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
             <div className="container-fluid input-card">
                 <div className="row my-2">
-                    <h5 className="m-0 p-0 mb-1">Url:</h5>
-                    {bulkListing ?
-                        <textarea className="form-control" placeholder="url list" id="url_list" rows="5" title="Url List" hidden="" value={urlList} onChange={updateUrls} /> :
-                        <input type="text" className="form-control" placeholder="url" id="url" data-bs-toggle="tooltip" data-bs-placement="top" title="Url" value={url} onChange={updateUrls} />}
+                    <h5 className="m-0 p-0 mb-1">Urls</h5>
+                    <textarea className="form-control unified-input" rows={urlList.split("\n").length < 12 ? urlList.split("\n").length : 12}
+                        placeholder="url list" id="url_list" title="Url List"
+                        value={urlList} onChange={updateUrls} />
                 </div>
                 <div className="row my-2">
                     <div className="col align-items-center m-0 p-0">
                         <div className="m-0 p-0 d-flex flex-row align-items-center">
-                            <label className="form-check-label emoji align-items-center">Watch Playlist? </label>
+                            <label className="form-check-label emoji align-items-center">Watch? </label>
                             <FormControl
                                 className="form-select-sm mx-2 align-items-center"
                                 id="watch-change"
@@ -118,23 +102,16 @@ export default function InputForm({ setParentUrl, setRespStart }) {
                             </FormControl>
                         </div>
                     </div>
-                    <div className="col align-items-center m-0 p-0">
-                        <div className="m-0 p-0 d-flex flex-row align-items-center">
-                            <label className="form-check-label emoji align-items-center">Toggle To: </label>
-                            <button id="bulk_tgl_btn" type="button" onClick={() => toggleBulk(!bulkListing)}
-                                className="btn btn-primary mx-2 btn-sm">{bulkListing ? "Single" : "Bulk"}</button>
-                        </div>
-                    </div>
                 </div>
                 <div className="row mb-2">
-                    <h5 className="m-0 p-0 mb-1">Controls:</h5>
+                    <h5 className="m-0 p-0 mb-1">Controls</h5>
                     <ListControl start={start} stop={stop} chunk={chunk} setStart={setStart} setStop={setStop} setChunk={setChunk} />
                 </div>
                 <div className="row mb-2">
                     <div className="col m-0 p-0">
                         <div className="btn-group" role="group">
                             <button id="list_btn" type="button" onClick={listThis} className="btn btn-primary">List</button>
-                            <button id="clear_btn" type="button" onClick={() => { console.log("TODO") }} className="btn btn-primary">Clear</button>
+                            <button id="clear_btn" type="button" onClick={clearInput} className="btn btn-primary">Clear</button>
                         </div>
                     </div>
                     <div className="col m-0 p-0"></div>
