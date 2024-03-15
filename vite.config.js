f6fda340a2c4f25f5a2219d27bb2e9f0e4e031d5
Sagnik Das <mailsagnikdas53@gmail.com>
2024-03-16 01:23:12 +0530

    added compression to build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+"use strict";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({}),
+    compression({
+      algorithm: 'brotliCompress',
+      exclude: [/\.(br)$/, /\.(gz)$/],
+      deleteOriginalAssets: true
+    }),
+  ],
 })
